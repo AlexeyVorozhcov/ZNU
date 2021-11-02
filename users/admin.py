a43b528e139a38f9855d5be549c93800c2ccde38
Alexey Vorozhtcov <alexey.vorozhcov@gmail.com>
2021-11-02 15:09:57 +0300

    add new model and disibg

--- users/admin.py
+++ users/admin.py
@@ -3,9 +3,14 @@ from users.models import User, Shops, Roles, Category
 
 # Register your models here.
 
-admin.site.register(User)
+# admin.site.register(User)
 admin.site.register(Shops)
 admin.site.register(Roles)
 admin.site.register(Category)
+
+@admin.register(User)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("username","role", "shop", "email")
+
 
 
--- zayavki/admin.py
+++ zayavki/admin.py
@@ -4,4 +4,11 @@ from zayavki.models import Zayavka
 # Register your models here.
 
 
-admin.site.register(Zayavka)
+# admin.site.register(Zayavka)
+
+@admin.register(Zayavka)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name","user", "category", "data")
+    fields = ("user", ("code", "name", "category"), "description", "clarification",
+    "foto1", "foto2", ("status1", "status2", "status3","status4", "status5", "status6"), 
+    "clarification_of_manager")
