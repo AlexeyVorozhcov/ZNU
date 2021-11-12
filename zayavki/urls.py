77b310e0da8871fc4a3a3af84155b3247196e43b
AlexeyVorozhcov <alexey.vorozhcov@gmail.com>
2021-11-12 12:41:36 +0300

    optimizated views zayavki

--- zayavki/urls.py
+++ zayavki/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from zayavki.views import page_view, add_zayavka, ZayavkaUpdate, ZayavkaDetail
+from zayavki.views import ZayavkaList, ZayavkaUpdate, ZayavkaDetail, ZayavkaCreate, ZayavkaFilterList
 
 
 
 app_name = "zayavki"
 
 urlpatterns = [
-    path("", page_view, name="page_view"),
-    # path('<str:filter>/', page_view, name='page_view_filter'),
+    path("", ZayavkaList.as_view(), name="zayavki_list"),
+    path('<str:filter>/', ZayavkaFilterList.as_view(), name='zayavki_filter_list'),
     # path('page/<int:page>/', page_view, name='page'),
-    path("add_zayavka", add_zayavka, name="add_zayavka"),
-    # path("add_zayavka", ZayavkaCreate.as_view(), name="add_zayavka"),
+    path("create", ZayavkaCreate.as_view(), name="zayavka-create"),
     path("<int:pk>", ZayavkaDetail.as_view(), name="zayavka-detail"),
-    path("<int:pk>/update", ZayavkaUpdate.as_view(), name="zayavka-update")
+    path("<int:pk>/update", ZayavkaUpdate.as_view(), name="zayavka-update"),
+    
 ]
--- zayavki/views.py
+++ zayavki/views.py
@@ -5,78 +5,125 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse
-from django.views.generic import UpdateView, DetailView, CreateView
+from django.views.generic import UpdateView, DetailView, CreateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-KOL_RECORDS_ON_PAGE = 10
+KOL_RECORDS_ON_PAGE = 50
 DICT_OF_FILTERS = {
-            "Все активные": "all",
-            "Ждут рассмотрения": "resh",
-            "Ждут уценки в 1С": "utc",
-            "Ждут уценки на витрине": "utc_inshop",
-            "Отклоненные": "otkl",
-            "Архивные": "arch"}
+    "Все активные": "all",
+    "Ждут рассмотрения": "resh",
+    "Ждут уценки в 1С": "utc",
+    "Ждут уценки на витрине": "utc_inshop",
+    "Отклоненные": "otkl",
+    "Архивные": "arch"}
+
+
+class ZayavkaCreate(CreateView):
+
+    model = Zayavka
+    template_name = "zayavki/zayavka_create.html"
+    form_class = AddZayavkaForm
+
+    def form_valid(self, form):
+        # Добавить текущего пользователя, кто создал заявку
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        # Добавить данные в контекст, передаваемый в шаблон
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Новая заявка"
+        context["name_page"] = "Новая заявка"
+        return context
+
+    def get_success_url(self):
+        # Перенаправить после успешного создания заявки
+        return reverse('zayavki:zayavki_list')
+
 
 class ZayavkaDetail(DetailView):
     model = Zayavka
-    template_name = "zayavki/zayavka_detail.html"
-    context_object_name = "zayavka"
-    # TODO добавить проверку существования заявки
-    
+    template_name = "zayavki/zayavka_create.html"
+
 
 class ZayavkaUpdate(UpdateView):
     model = Zayavka
-    template_name = "zayavki/add_zayavka.html"    
+    template_name = "zayavki/zayavka_create.html"
     form_class = AddZayavkaForm
 
-# class ZayavkaCreate(CreateView):
-#     model = Zayavka
-#     template_name = "zayavki/ADD_ZAYAVKA.html"
-#     form_class = AddZayavkaForm
 
-    # def form_valid(self, form):
-    #         form.instance.created_by = self.request.user
-    #         return super(MyModelAdd, self).form_valid(form)
+class ZayavkaList(ListView):
+    model = Zayavka
+    #paginate_by = 50
+    template_name = "zayavki/zayavka_list.html"
+    context_object_name = "zayavki"
 
-@login_required()
-def page_view(request):
-    '''
-    В GET-запросе могут прийти параметры page и/или filter. 
-    '''
-    template = "zayavki/page_view.html"  # шаблон страницы
-    page = request.GET.get('page', None) # выдергиваем из запроса page; если его нет - None
-    cur_filter = request.GET.get('filter', None) # выдергиваем из запроса filter; если его нет - None
-    if not cur_filter:
-        # если filter не передан, или передано несуществующее значение, устанавливается фильтр
-        # по умолчанию в зависимости от роли пользователя
-        cur_filter = get_default_filter(user=request.user)
-    if not page:
-        # если page не передан или передано некорректное значение,
-        # устанавливается первая страница пагинатора
-        # TODO добавить проверку на page=положительное число
-        page = 1
-    
-    context = {
-        "filters": DICT_OF_FILTERS,
-        "cur_filter": cur_filter,
-        "title": "Заявки на уценку",        
-        "data_from_model_Zayavka": get_data_from_model_Zayavka(filter=cur_filter, user=request.user, page=page)
-    }
-    return render(request, template, context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Заявки на уценку"
+        context["name_page"] = "Заявки на уценку"
+        context['filters'] = DICT_OF_FILTERS
+        context['cur_filter'] = self.get_default_filter()
+        return context
+
+    def get_queryset(self):
+        return get_data_from_model_Zayavka(self.get_default_filter(), self.request.user, 1)
+
+    def get_default_filter(self):
+        if self.request.user.role.namerole == "Магазин":
+            return DICT_OF_FILTERS["Ждут уценки на витрине"]
+        elif self.request.user.role.namerole[:3] == "Мен":
+            return DICT_OF_FILTERS["Ждут рассмотрения"]
+        else:
+            return DICT_OF_FILTERS["Все активные"]
 
 
-def get_default_filter(user):
-    '''
-    Возвращает стартовый фильтр. Если вход без фильтра, то устанавливается стартовый фильтр в зависимости от
-    роли пользователя. Если это магазин - стартовый фильтр будет 'Ждет уценки на витрине', 
-    если менеджер = 'Ждут рассмотрения'.
-    Возвращает исходный фильтр, если он не пустой
-    '''
-    if user.role.namerole == "Магазин":
-        return DICT_OF_FILTERS["Ждут уценки на витрине"]
-    if user.role.namerole[:3] == "Мен":
-        return DICT_OF_FILTERS["Ждут рассмотрения"]
-    
+class ZayavkaFilterList(ListView):
+    model = Zayavka
+    #paginate_by = 50
+    template_name = "zayavki/zayavka_list.html"
+    context_object_name = "zayavki"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Заявки на уценку"
+        context["name_page"] = "Заявки на уценку"
+        context['filters'] = DICT_OF_FILTERS
+        context['cur_filter'] = self.kwargs['filter']
+        return context
+
+    def get_queryset(self):
+        return get_data_from_model_Zayavka(self.kwargs['filter'], self.request.user, 1)
+
+
+# @login_required()
+# def page_view(request):
+#     '''
+#     В GET-запросе могут прийти параметры page и/или filter.
+#     '''
+#     template = "zayavki/zayavki_list.html"  # шаблон страницы
+#     page = request.GET.get('page', None) # выдергиваем из запроса page; если его нет - None
+#     cur_filter = request.GET.get('filter', None) # выдергиваем из запроса filter; если его нет - None
+#     if not cur_filter:
+#         # если filter не передан, или передано несуществующее значение, устанавливается фильтр
+#         # по умолчанию в зависимости от роли пользователя
+#         cur_filter = get_default_filter(user=request.user)
+#     if not page:
+#         # если page не передан или передано некорректное значение,
+#         # устанавливается первая страница пагинатора
+#         # TODO добавить проверку на page=положительное число
+#         page = 1
+
+#     context = {
+#         "filters": DICT_OF_FILTERS,
+#         "cur_filter": cur_filter,
+#         "title": "Заявки на уценку",
+#         "data_from_model_Zayavka": get_data_from_model_Zayavka(filter=cur_filter, user=request.user, page=page)
+#     }
+#     return render(request, template, context)
+
+
 def get_data_from_model_Zayavka(filter, user, page):
     '''
     Возвращает выборку из таблицы по условиям фильтра. Если пользователь Магазин - выборка записей этого магазина. 
@@ -88,11 +135,12 @@ def get_data_from_model_Zayavka(filter, user, page):
     bd_for_user = None
     # если Магазин, то отбираются все заявки с этим магазином
     if user.role.namerole == "Магазин":
-        bd_for_user = Zayavka.objects.filter(user__shop=user.shop).order_by("-id")
+        bd_for_user = Zayavka.objects.filter(
+            user__shop=user.shop).order_by("-id")
     else:
         bd_for_user = Zayavka.objects.filter(
             category__in=user.role.work_category.all()).order_by("-id")
-    
+
     arch = bd_for_user.filter(status5=True)  # архивные
     all_z = bd_for_user.filter(status5=False)  # все кроме архивных
     resh = all_z.filter(status1=False, status2=False)  # ожидающие решения
@@ -119,28 +167,29 @@ def get_data_from_model_Zayavka(filter, user, page):
     return result_paginator
 
 
-@login_required
-def add_zayavka(request):
-    '''
-    Просмотр(GET) или создание(POST) заявки.
-    Трабл - программа не изменяет заявки, а создает новые со теми же полями, с новым id
+# @login_required
+# def add_zayavka(request):
+#     '''
+#     Просмотр(GET) или создание(POST) заявки.
+#     Трабл - программа не изменяет заявки, а создает новые со теми же полями, с новым id
 
-    '''
-    name_page = None    
-    if request.method == "POST":
-        form = AddZayavkaForm(data=request.POST, files=request.FILES, initial={'user': request.user})
-        if form.is_valid(): 
-            added_zayavka = form.save() 
-            added_zayavka.user = request.user
-            added_zayavka.save()
-                       
-        else:
-            print (form.errors)
-        return HttpResponseRedirect(reverse('zayavki:page_view'))
- 
-    form = AddZayavkaForm()    
-    template = "zayavki/add_zayavka.html"
-    context = {"form": form,
-               "title": name_page,
-               "name_page" : name_page}
-    return render(request, template, context)
+#     '''
+#     name_page = None
+#     if request.method == "POST":
+#         form = AddZayavkaForm(data=request.POST, files=request.FILES, initial={
+#                               'user': request.user})
+#         if form.is_valid():
+#             added_zayavka = form.save()
+#             added_zayavka.user = request.user
+#             added_zayavka.save()
+
+#         else:
+#             print(form.errors)
+#         return HttpResponseRedirect(reverse('zayavki:zayavki_list'))
+
+#     form = AddZayavkaForm()
+#     template = "zayavki/add_zayavka.html"
+#     context = {"form": form,
+#                "title": name_page,
+#                "name_page": name_page}
+#     return render(request, template, context)
